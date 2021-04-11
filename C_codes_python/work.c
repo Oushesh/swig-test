39e4967dd0f0ad28b5c384e927aa2703b554fbc1
oushesh <oushesh@gmail.com>
2021-04-11 10:57:01 +0200

    added C_codes_python

--- C_codes_python/work.c
+++ C_codes_python/work.c
@@ -5,5 +5,45 @@ Using c codes in python|Set 1
 
 int gcd(int x, int y)
 {
-  
+  int g=y;
+  while (x>0)
+  {
+    g=x;
+    x=y%x;
+    y=g;
+  }
+  return g;
 }
+
+int divide(int a, int b, int * remainder)
+{
+  int quot = a/b;
+  *remainder = a%b;
+  return quot;
+}
+
+double avg(double *a, int n)
+{
+  double total = 0.0;
+  for (int i=0;i<n;i++)
+  {
+    total = a[i];
+  }
+  return total/n;
+}
+
+typdef struct Point
+{
+  double x,y;
+}
+Point;
+
+double distance(Point *p1, Point *p2)
+{
+  return hypot(p1->x-p2->x,p1->y-p2->y);
+}
+
+
+//Take the code and compile it into a library.
+//Link to other code.
+a
